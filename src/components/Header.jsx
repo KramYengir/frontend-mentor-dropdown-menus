aee318e410923baf1ee05a969f5f65a99d12d70f
KramYengir <kramyengir@gmail.com>
2024-01-18 13:29:29 +0000

    set header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
 const Header = () => {
   return (
     <header>
-      <div className="container">Header</div>
+      <div className="container">
+        <h1 className="logo">snap</h1>
+        <nav>
+          <ul>
+            <li>
+              <div className="dropdown-btn">
+                <button>Features</button>
+              </div>
+            </li>
+            <li>
+              <div className="dropdown-btn">
+                <button>Company</button>
+              </div>
+            </li>
+            <li>Careers</li>
+            <li>About</li>
+          </ul>
+          <div className="user-btns">
+            <button>Login</button>
+            <button>Register</button>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
