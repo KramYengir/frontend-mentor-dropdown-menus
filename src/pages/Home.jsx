322deb1e82218214a315e43fbe4ec67164bbf717
KramYengir <kramyengir@gmail.com>
2024-01-19 17:34:31 +0000

    challenge complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,37 @@ import audiophileIMG from "../assets/images/client-audiophile.svg";
 import databizIMG from "../assets/images/client-databiz.svg";
 import makerIMG from "../assets/images/client-maker.svg";
 import meetIMG from "../assets/images/client-meet.svg";
-import heroIMG from "../assets/images/image-hero-desktop.png";
+import heroDeskIMG from "../assets/images/image-hero-desktop.png";
+import heroMobileIMG from "../assets/images/image-hero-mobile.png";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Update the viewport width when the window is resized
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array ensures that the effect runs only once on mount
+
   return (
     <main>
       <div className="container hero">
         <div className="information">
           <div className="cta">
-            <h1>Make remote work</h1>
+            <h1>
+              Make <br />
+              remote work
+            </h1>
             <p>
               {" "}
               Get your team in sync, no matter your location. Streamline
@@ -26,7 +48,10 @@ const Home = () => {
           </div>
         </div>
         <div className="hero__image">
-          <img src={heroIMG} alt="hero image of blcak man with a laptop" />
+          <img
+            src={viewportWidth < 1000 ? heroMobileIMG : heroDeskIMG}
+            alt="hero image of blcak man with a laptop"
+          />
         </div>
       </div>
     </main>
